9cf0467d7c0ab1a2472d42ad13d1c97bb9d7d83f
Ayşe Sude Karabal <karabalayssee@gmail.com>
2025-11-23 17:46:00 +0300

    1. hafta
    profiles.py içerisine eklemeler yapıldı, dosyalar oluşturuldu.

---
+++ profiles.py
@@ -0,0 +1,40 @@
+import json
+
+DEFAULT_USER_PATH = "users.json"
+
+def load_users(path: str):
+    f = open(path)
+    print(f.read())
+    #json dosyasından user infoları alıp yazdıracak
+
+def save_users(path: str, users: list):
+    
+    with open(path, "a") as f:
+        f.write(users)
+        #users listesini verilen konuma kaydedecek??
+    
+def register_user(users: list, profile: dict):
+    newuser = {
+        #buraya profilden veri çekecek bir dictionary yazılması lazım
+    }
+    users.append(newuser)
+    return newuser
+
+
+def authenticate_user(users: list, email: str, pin: str):
+    for user in users:
+        if user.get("email") == email and user.get("pin") == pin:
+             #kullanıcı bulundu
+            return user
+        else:
+            #kullanıcı bulunamadı
+            print("No user with that name or id.")
+            return None
+
+
+def update_goal(users: list, user_id: str, goal_data: dict):
+    for user in users:
+        # id ile uyuşan kullanıcıyı bulup onun goal'una erişmeliyiz
+        if user_id == user.get("user_id"):
+            user["goal"].update (goal_data)
+
